ba43887eb541a88406891282cd0ab0786fb9dbd7
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2023-08-31 12:19:43 -0300

    [feat] implemented attendant entity

---
+++ src/main/java/br/com/dev/clinica/domain/attendant/Attendant.java
@@ -0,0 +1,53 @@
+package br.com.dev.clinica.domain.attendant;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "attendants")
+@Entity(name = "attendants")
+public class Attendant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false)
+    private String id;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column(nullable = false, length = 30)
+    private String first_name;
+
+    @Column(nullable = false, length = 50)
+    private String last_name;
+
+    @Column(nullable = false)
+    private Date birthdate;
+
+    @Column(nullable = false, length = 100)
+    private String street;
+
+    @Column(nullable = false)
+    private Integer number;
+
+    @Column(nullable = false, length = 30)
+    private String city;
+
+    @Column(nullable = false, length = 2)
+    private String uf;
+
+    @Column(nullable = false, length = 16)
+    private String cellphone;
+
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @Column(nullable = false)
+    private boolean isActive;
+}
