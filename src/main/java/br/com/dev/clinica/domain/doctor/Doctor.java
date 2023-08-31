ac71416f09f9d54c2e0c995bd99cc6ed84ebb360
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2023-08-31 10:59:38 -0300

    [feat] created doctor entity

---
+++ src/main/java/br/com/dev/clinica/domain/doctor/Doctor.java
@@ -0,0 +1,59 @@
+package br.com.dev.clinica.domain.doctor;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "doctors")
+@Entity(name = "doctors")
+public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false)
+    private String id;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column(nullable = false, unique = true)
+    private String crm;
+
+    @Column(nullable = false, length = 30)
+    private String first_name;
+
+    @Column(nullable = false, length = 50)
+    private String last_name;
+
+    @Column(nullable = false)
+    private Date birthdate;
+
+    @Column(nullable = false, length = 100)
+    private String street;
+
+    @Column(nullable = false)
+    private Integer number;
+
+    @Column(nullable = false, length = 30)
+    private String city;
+
+    @Column(nullable = false, length = 2)
+    private String uf;
+
+    @Column(nullable = false, length = 16)
+    private String cellphone;
+
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @Column(nullable = false)
+    private String[] specialties;
+
+    @Column(nullable = false)
+    private boolean isActive;
+}
