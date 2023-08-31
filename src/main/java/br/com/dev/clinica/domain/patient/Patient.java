4a5821816f1edacbd6ca7bb26141eea4244cbe1b
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2023-08-31 10:17:02 -0300

    [feat] created patient entity

---
+++ src/main/java/br/com/dev/clinica/domain/patient/Patient.java
@@ -0,0 +1,53 @@
+package br.com.dev.clinica.domain.patient;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "patients")
+@Entity(name = "patients")
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false)
+    private String id;
+
+    @Column(nullable = false, length = 30)
+    private String first_name;
+
+    @Column(nullable = false, length = 50)
+    private String last_name;
+
+    @Column(nullable = false)
+    private Date birthdate;
+
+    @Column(nullable = false, length = 100)
+    private String street;
+
+    @Column(nullable = false)
+    private Integer number;
+
+    @Column(nullable = false, length = 30)
+    private String city;
+
+    @Column(nullable = false, length = 2)
+    private String uf;
+
+    @Column(nullable = false, length = 16)
+    private String cellphone;
+
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 30)
+    private String health_insurance;
+
+    @Column(nullable = false, length = 50)
+    private String health_insurance_code;
+}
